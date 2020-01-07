ad6ac41079b296dfc8fc52ebb6b6a6f6f069c790
Nina Angelo <ninacelie@gmail.com>
2020-01-07 17:06:33 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -14,7 +14,7 @@ class Song
   end
 
   def self.new_by_filename(file)
-    song = self.new 
+    song = self.new
     song_name = file.split(" - ")[1]
     song.artist_name = file.split(" - ")[0]
     song.artist = Artist.find_or_create_by_name(name)
