745da39388be6a349026e8a7adf692a14d7fdc66
Nina Angelo <ninacelie@gmail.com>
2020-01-07 17:28:27 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -35,26 +35,15 @@ class Artist
     @@all << self
   end
 
-  # def self.find_or_create_by_name(artist_name)
-  #   found_artist = self.all.find {|artist| artist.name == artist_name}
-  #   if found_artist
-  #     found_artist
-  #   else
-  #     new_artist = self.new(artist_name)
-  #     new_artist.save
-  #     new_artist
-  #   end
-  # end
-
   def self.find_or_create_by_name(name)
-    if self.find(name)
-     self.find(name)
-   else
-     self.create(name)
-   end
- end
-
-  def print_songs
-    self.songs.each {|song| puts song.name}
+  if self.find(name)
+    self.find(name)
+  else
+    self.create(name)
   end
+end
+
+def print_songs
+  self.songs.each {|song| puts song.name}
+end
 end
