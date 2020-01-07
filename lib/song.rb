e31a2680fde643a13f9960b8ab0db69197ab9219
Nina Angelo <ninacelie@gmail.com>
2020-01-07 17:36:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -13,9 +13,9 @@ class Song
     @@all
   end
 
-  def self.new_by_filename(filename)
-    song_info = filename.chomp(".mp3").split(" - ")
-    song = Song.new(song_info[1])
+  def self.new_by_filename(file)
+    song_info = file.chomp(".mp3").split(" - ")
+    song = Song.new(song_info)[1]
     song.artist_name = song_info[0]
     song
     end
