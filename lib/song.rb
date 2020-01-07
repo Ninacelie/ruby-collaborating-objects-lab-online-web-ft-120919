068838e3b57c8865d25b506d6b04222e2b1519b0
Nina Angelo <ninacelie@gmail.com>
2020-01-07 17:10:15 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -26,7 +26,5 @@ class Song
     artist = Artist.find_or_create_by_name(name)
     self.artist = artist
     artist.add_song(self)
-      # self.artist = Artist.find_or_create_by_name(name)
-      # self.artist.add_song(self)
   end
 end
